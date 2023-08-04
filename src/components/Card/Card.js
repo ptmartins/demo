a034e531581bf69823491f35b938e52288c72682
Pedro Martins <pedro.martins@imagen.io>
2023-08-04 12:22:12 +0100

    Add build

---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import styles from './Card.module.css';
+
+
+function Card() {
+    return(
+        <div className={styles.card}>
+            <h1>Card</h1>
+        </div>
+    )
+}
+
+export default Card;
